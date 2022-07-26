2a211d2ef3636045a7760a6f82cbb9de6a6b8931
Vincedinardo21 <vincedinardo21@gmail.com>
2022-07-26 18:13:28 +0200

    Fine posts

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,16 +1,22 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        //
+        $users = [
+            [
+                'name' => 'Maurizio',
+                'email' => 'maurizio@gmail.com',
+                'password' => Hash::make('asdfasdf'),
+            ],
+        ];
+
+        foreach($users as $user){
+            User::create($user);
+        }
     }
 }
