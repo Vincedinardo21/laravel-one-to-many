607eb815a2fb644d607b3495b22914d7004697a2
Vincedinardo21 <vincedinardo21@gmail.com>
2022-07-26 16:41:17 +0200

    Add table posts

--- routes/web.php
+++ routes/web.php
@@ -28,5 +28,6 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function(){
         Route::get('/', 'AdminController@dashboard')->name('dashboard');
+        // Route::get('/temp', 'AdminController@temp')->name('temp');
         Route::resource('posts', 'PostController');
     });
